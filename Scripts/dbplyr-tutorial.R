35f10a055b00d66fc99036dd211611b16de2e449
njlyon0 <nicholasjlyon@gmail.com>
2022-04-11 13:40:29 -0700

    Finished chapter 1 (chap 2 added previously) of the tutorial

--- Scripts/dbplyr-tutorial.R
+++ Scripts/dbplyr-tutorial.R
@@ -4,19 +4,116 @@
 
 # This contains a `dbplyr` tutorial
 
-# Clear environment
-rm(list = ls())
-
 # Load these libraries
 library(tidyverse); library(DBI); library(RSQLite)
+
+# Clear environment
+rm(list = ls())
 
 # Tutorial #1: R, Databases & SQL ----------------------------------
 
 # T1 - Chapter 1: DBI ----------------------------------------------
 # Link: rdbsql.rsquaredacademy.com/dbi.html
 
+# Connect 
+con <- dbConnect(RSQLite::SQLite(), ":memory:")
 
+# Summary of connection
+summary(con)
 
+# Copy mtcars to database
+dplyr::copy_to(con, mtcars)
+
+# See which tables are included in the database
+dbListTables(con)
+
+# List fields (i.e., columns) in that element of the database
+dbListFields(conn = con, name = "mtcars")
+
+# We can examine everything in a given element of the database
+dbReadTable(conn = con, name = 'mtcars')
+
+# Query database using SQL commands
+dbGetQuery(conn = con, statement = "SELECT * FROM mtcars LIMIT 10")
+
+# Can seperately query and then specify how many rows to return
+## Query
+query <- dbSendQuery(conn = con, 'SELECT * FROM mtcars')
+## Get result
+result <- dbFetch(res = query, n = 5)
+result
+
+# Can check query status (useful for queries that take a long time to process)
+dbHasCompleted(res = query)
+
+# Can also get more complete information
+dbGetInfo(dbObj = query)
+
+# Return most recent query:
+dbGetStatement(res = query)
+
+# How many rows were requested
+dbGetRowCount(res = query)
+
+# See how many rows are modified by query
+dbGetRowsAffected(res = query)
+
+# See column names and format content
+dbColumnInfo(res = query)
+
+# Can create a table and put it into an existing database as a new element
+x <- 1:10
+y <- letters[1:10]
+trial <- tibble::tibble(x, y)
+dbWriteTable(conn = con, name = "trial", value = trial)
+
+# Check that the data got added and look right
+dbListTables(conn = con)
+dbGetQuery(conn = con, statement = "SELECT * FROM trial LIMIT 3")
+dbExistsTable(conn = con, name = "trial")
+
+# Can also overwrite a given table in a database
+x <- sample(100, 10)
+y <- letters[11:20]
+trial2 <- tibble::tibble(x, y)
+dbWriteTable(conn = con, name = "trial", value = trial2, overwrite = T)
+
+# Check it
+dbListTables(conn = con)
+dbGetQuery(conn = con, statement = "SELECT * FROM trial LIMIT 3")
+
+# Can also append additional information to an existing table
+x <- sample(100, 10)
+y <- letters[5:14]
+trial3 <- tibble::tibble(x, y)
+dbWriteTable(conn = con, name = "trial", value = trial3, append = T)
+dbReadTable(conn = con, name = "trial")
+
+# Can insert rows into an existing table
+## Method #1
+dbExecute(conn = con,
+          statement = "INSERT into trial (x, y)
+          VALUES (32, 'c'), (45, 'k'), (61, 'h')")
+
+## Method #2 (prints some diagnostic info upon completion)
+dbSendStatement(conn = con,
+                statement = "INSERT into trial (x, y)
+                VALUES (25, 'm'), (54, 'l'), (16, 'y')")
+
+# Deleting a table is also possible
+dbRemoveTable(conn = con, name = "trial")
+dbListTables(conn = con)
+
+# Want to check the data type?
+dbDataType(dbObj = RSQLite::SQLite(), obj = "a")
+dbDataType(dbObj = RSQLite::SQLite(), obj = 1:5)
+dbDataType(dbObj = RSQLite::SQLite(), obj = 1.5)
+
+# End by closing connection
+dbDisconnect(con)
+
+# Clear environment
+rm(list = ls())
 
 # T1 - Chapter 2: dbplyr ---------------------------------------------------
 # Link: rdbsql.rsquaredacademy.com/dbplyr.html
@@ -61,6 +158,9 @@ dplyr::collect(mileages)
 
 # When done with connection:
 dbDisconnect(con)
+
+# Clear environment
+rm(list = ls())
 
 # T1 - Chapter 3: SQL Basics --------------------------------------------
 # Link: rdbsql.rsquaredacademy.com/sqlbasics.html
