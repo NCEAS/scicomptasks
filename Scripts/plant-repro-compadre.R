090e70af5891172f3514c770b225a6aaa02390eb
njlyon0 <nicholasjlyon@gmail.com>
2022-04-25 13:59:14 -0700

    First pass at exploring COMPADRE database (population projection matrices) for the plant reproduction working group

---
+++ Scripts/plant-repro-compadre.R
@@ -0,0 +1,31 @@
+## ----------------------------------------------------------------- ##
+              # Plant Reproduction - COMPADRE Exploration
+## ----------------------------------------------------------------- ##
+# Written by Nick J Lyon
+
+# PURPOSE
+## Explore plant population projection matrices included in COMPADRE
+## Link here: compadre-db.org/Data
+
+# Clear environment
+rm(list = ls())
+
+# Call packages
+library(tidyverse)
+
+# Load data
+load(file.path("Data", "COMPADRE_v.6.22.1.2.RData"))
+
+# Housekeeping ------------------------------------------------
+
+# Strip out species information
+spp <- compadre[["metadata"]] %>%
+  dplyr::select(OrganismType, Genus, Species) %>%
+  dplyr::filter(OrganismType == "Tree") %>%
+  unique()
+
+# Check it out
+
+
+
+# End ---------------------------------------------------------
