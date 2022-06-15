324668c1c5fce73fa9154de63ca7b389b7c7fc68
njlyon0 <nicholasjlyon@gmail.com>
2022-06-15 10:17:08 -0500

    Added more informative comments and re-formatted headers

--- wg_plant_repro/plant-repro-compadre.R
+++ wg_plant_repro/plant-repro-compadre.R
@@ -1,6 +1,6 @@
-## ----------------------------------------------------------------- ##
-              # Plant Reproduction - COMPADRE Exploration
-## ----------------------------------------------------------------- ##
+## -------------------------------------------------------- ##
+         # Plant Reproduction - COMPADRE Exploration
+## -------------------------------------------------------- ##
 # Written by Nick J Lyon
 
 # PURPOSE
@@ -14,42 +14,49 @@ rm(list = ls())
 library(tidyverse)
 
 # Load COMPADRE data
-load(file.path("Data", "COMPADRE_v.6.22.1.2.RData"))
+load(file.path("data", "COMPADRE_v.6.22.1.2.RData"))
 
 # Load working group data
 wg_raw <- read.csv(file.path("Data", "LTER_SEED_SPP_ATTRIBUTES_v1.csv"))
 
-# Housekeeping ------------------------------------------------
+# Wrangling ----------------------------------------
 
-# Strip out species information
-## COMPADRE
+# Wrangle COMPADRE metadata
 compadre_spp <- compadre[["metadata"]] %>%
+  # Retain only genus/species
   dplyr::select(Genus, Species) %>%
+  # Retain only unique values
+  unique() %>%
+  # Paste them together into a single column
   dplyr::mutate(genus_spp = paste(Genus, Species, sep = '-'),
-                in_compadre = 1) %>%
-  # dplyr::filter(OrganismType == "Tree") %>%
-  unique()
-## Working group list
+                in_compadre = 1)
+
+# Wrangle Plant Reproduction WG species list
 wg_spp <- wg_raw %>%
+  # Keep only 'species' (is actually 'Genus species') column
   dplyr::select(species) %>%
+  # Remove empty rows
   dplyr::filter(nchar(species) != 0) %>%
+  # Separate genus and species
   tidyr::separate(col = species, sep = " ", into = c("Genus", "Species")) %>%
+  # Recombine them with our desired separator
   dplyr::mutate(genus_spp = paste(Genus, Species, sep = '-'))
 
-# Compare Working Group List vs. COMPADRE List ---------------------------
+# Compare Working Group List vs. COMPADRE List ---------------
 
 # Combine them to figure out which WG species are in COMPADRE
 combo <- wg_spp %>%
-  # Grab compadre data to determine which COMPADRE species are in data
+  # Left join together to see which COMPADRE species are in data
   dplyr::left_join(y = compadre_spp, by = "Genus") %>%
   # Remove unneeded column
   dplyr::select(-'Species.y') %>%
-  # Get all congeners into their own columns
+  # Get all congeners (spp in the same genus) into their own columns
   tidyr::pivot_wider(id_cols = genus_spp.x,
                      names_from = genus_spp.y,
                      values_from = genus_spp.y) %>%
-  # Unite all those new columns into a single 'congeners' column
-  tidyr::unite(col = "congeners", -genus_spp.x, sep = '; ', na.rm = T) %>%
+  # Unite all those new columns into a single congeners column
+  tidyr::unite(col = "congeners", -genus_spp.x,
+               sep = '; ', na.rm = T) %>%
   # Fix a naming issue caused by the join
   dplyr::rename(genus_spp = genus_spp.x) %>%
   # Bring the Genus/Species back in
@@ -70,6 +77,7 @@ plyr::count(combo$sp_in_compadre)
 plyr::count(combo$genus_in_compadre)
 
 # Save this out
-write.csv(x = combo, row.names = F, file = file.path("Data", "PlantReproList_vs_COMPADRE.csv"))
+write.csv(x = combo, row.names = F,
+          file = file.path("data", "PlantReproList_vs_COMPADRE.csv"))
 
 # End ---------------------------------------------------------
