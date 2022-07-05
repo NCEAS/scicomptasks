973bfdc8a66aa14c5415046510d337d599637d3c
njlyon0 <nicholasjlyon@gmail.com>
2022-07-05 09:52:48 -0500

    Created a (rough draft) function that extracts SiZer output and wrangles it to gather inflection points

--- wg_silica/silica-SiZer-pkg-exploration.r
+++ wg_silica/silica-SiZer-pkg-exploration.r
@@ -55,7 +55,7 @@ e2 <- SiZer::SiZer(x = priscu$Year, y = priscu$FNYield,
 plot(e2)
 abline(h = 0.5)
 
-# Practice Manipulating SiZer Object -----
+# Extract Needed Data from SiZer Object -----
 # Do a side-by-side plot of trendline with SiZer plot
 par(mfrow = c(1, 3))
 plot(priscu$Year, priscu$FNYield)
@@ -106,9 +106,9 @@ sizer_df_v2 <- sizer_df %>%
   )) %>%
   # Lets also identify what type of change the transition was
   dplyr::mutate(change_type = dplyr::case_when(
-    transition == "change" & slope == 1 ~ 'zero-to-positive',
-    transition == "change" & slope == 0 ~ 'change-to-zero',
-    transition == "change" & slope == -1 ~ 'zero to negative')) %>%
+    transition == "change" & slope == 1 ~ 'zero_to_positive',
+    transition == "change" & slope == 0 ~ 'change_to_zero',
+    transition == "change" & slope == -1 ~ 'zero_to_negative')) %>%
   # End by ungrouping (good practice)
   dplyr::ungroup()
 
@@ -139,7 +139,8 @@ sizer_test <- sizer_transitions %>%
   dplyr::summarise(slope = dplyr::first(slope),
                    mean_x = mean(as.numeric(x_grid), na.rm = T),
                    sd_x = sd(as.numeric(x_grid), na.rm = T),
-                   se_x = sd_x / dplyr::n())
+                   n_x = dplyr::n(),
+                   se_x = sd_x / n_x)
 
 # Check out output
 head(sizer_test)
@@ -153,16 +154,163 @@ ggplot(sizer_test, aes(x = mean_x, y = slope)) +
   theme(axis.text.x = element_text(angle = 20, hjust = 1))
 
 # Now call the original plot of the trendline and add a line at these places
+## Graph the yield data
 ggplot(priscu, aes(x = Year, y = FNYield)) +
   geom_point() +
-  geom_smooth(method = 'loess', formula = 'y ~ x', se = F) +
-  geom_vline(xintercept = sizer_test$mean_x) +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 20, hjust = 1))
+  geom_smooth(method = 'loess', formula = 'y ~ x', se = F, color = 'black') +
+  # Add an orange line for the lower SE of each inflection point
+  geom_vline(xintercept = (sizer_test$mean_x - sizer_test$se_x),
+             color = 'orange') +
+  # Add a blue line for the *upper* SE of each inflection pt
+  geom_vline(xintercept = (sizer_test$mean_x + sizer_test$se_x),
+             color = 'blue') +
+  # Add pale blue rectangles for the standard *deviation* of each inflection point
+  annotate(geom = 'rect', xmin = (sizer_test$mean_x - sizer_test$sd_x),
+           xmax = (sizer_test$mean_x + sizer_test$sd_x), ymin = -Inf,
+           ymax = Inf, alpha = 0.2, fill = 'blue') +
+  # Add one more line for the mean inflection point
+  geom_vline(xintercept = sizer_test$mean_x, color = 'white') +
+  # Clean up the theme
+  theme_classic()
 
 
+# Make a simpler version as well
+ggplot(priscu, aes(x = Year, y = FNYield)) +
+  geom_point() +
+  geom_smooth(method = 'loess', formula = 'y ~ x',
+              se = F, color = 'black') +
+  geom_vline(xintercept = sizer_test$mean_x, color = 'blue') +
+  theme_classic()
 
+# Identify (Singular) Inflection Point ----
 
+# Check out the object we'll use to calculate this inflection pt
+sizer_test
+
+# Want to divide distance between each in half
+sizer_singular <- sizer_test %>%
+  # Sort from lowest to highest X
+  dplyr::arrange(mean_x) %>%
+  # Calculate distance to next one
+  dplyr::mutate( dist_to_next = dplyr::lead(x = mean_x) - mean_x ) %>%
+  # Add half the distance between +/0 and 0/- to +/0 to get +/- inflection point
+  dplyr::mutate(
+    pos_to_neg = ifelse(change_type == "change_to_zero" & dplyr::lead(change_type) == "zero_to_negative",
+                 yes = (mean_x + (dist_to_next / 2)),
+                 no = NA) )
+
+# Take a look!
+sizer_singular
+
+# Graph it
+ggplot(priscu, aes(x = Year, y = FNYield)) +
+  geom_point() +
+  geom_smooth(method = 'loess', formula = 'y ~ x',
+              se = F, color = 'black') +
+  geom_vline(xintercept = sizer_singular$pos_to_neg, color = 'blue') +
+  geom_vline(xintercept = sizer_test$mean_x, color = 'orange') +
+  theme_classic()
+
+# SiZer Extract Function ----
+
+# Create a function to streamline the above
+sizer_extract <- function(sizer_x = NULL, sizer_y = NULL, deriv = 1){
+  ## Argument explanation
+  # sizer_x = data$column specification of x-axis of trend line
+  # sizer_y = data$column specification of y-axis
+  # deriv = (numeric) specification of derivative for `SiZer::SiZer`
+
+  # Error out if arguments aren't specified
+  if(is.null(sizer_x) | is.null(sizer_y))
+    stop("All arguments must be specified")
+
+  # Error out for unsupported derivative
+  if(!as.numeric(deriv) %in% c(1, 2))
+    stop("Unsupported derivative! Must be either 1 or 2")
+
+  # Identify inflection points (for either derivative)
+  if(as.numeric(deriv) == 1){
+    sizer_mod <- SiZer::SiZer(x = sizer_x, y = sizer_y,
+                              h = c(2, 10), degree = 1,
+                              derv = 1, grid.length = 100) }
+  if(as.numeric(deriv) == 2){
+    sizer_mod <- SiZer::SiZer(x = sizer_x, y = sizer_y,
+                              h = c(2, 10), degree = 2,
+                              derv = 2, grid.length = 50) }
+
+  # Strip out the relevant information
+  sizer_raw <- as.data.frame(sizer_mod$slopes)
+  names(sizer_raw) <- sizer_mod$x.grid
+  sizer_raw$h_grid <- sizer_mod$h.grid
+
+  # Perform necessary wrangling
+  sizer_data <- sizer_raw %>%
+    # Pivot to long format to make it a little easier to scan through
+    tidyr::pivot_longer(cols = -h_grid,
+                        names_to = "x_grid",
+                        values_to = "slope") %>%
+    # Drop all 'insufficient data' rows
+    dplyr::filter(slope != 2) %>%
+    # Within bandwidth levels (h_grid)
+    dplyr::group_by(h_grid) %>%
+    # Identify whether the next value is the same or different
+    dplyr::mutate(transition = dplyr::case_when(
+      # First identify start of each group
+      is.na(dplyr::lag(slope, n = 1)) ~ 'start',
+      # is.na(dplyr::lead(slope, n = 1)) ~ 'end',
+      # Now identify whether each value is the same as or different than previous
+      slope == dplyr::lag(slope, n = 1) ~ 'same',
+      slope != dplyr::lag(slope, n = 1) ~ 'change'
+      # slope == dplyr::lead(slope, n = 1) ~ 'no'
+    )) %>%
+    # Lets also identify what type of change the transition was
+    dplyr::mutate(change_type = dplyr::case_when(
+      transition == "change" & slope == 1 ~ 'change_to_positive',
+      transition == "change" & slope == 0 ~ 'change_to_zero',
+      transition == "change" & slope == -1 ~ 'change_to_negative')) %>%
+    # Ungroup for subsequent operations
+    dplyr::ungroup() %>%
+    # Filter to retain only those rows that indicate a slope change
+    dplyr::filter(transition == "change") %>%
+    # Group by change type
+    dplyr::group_by(change_type) %>%
+    # And average the x_grid value
+    dplyr::summarise(slope = dplyr::first(slope),
+                     mean_x = mean(as.numeric(x_grid), na.rm = T),
+                     sd_x = sd(as.numeric(x_grid), na.rm = T),
+                     n_x = dplyr::n(),
+                     se_x = sd_x / n_x) %>%
+    # Ungroup
+    dplyr::ungroup() %>%
+    # Sort from lowest to highest X
+    dplyr::arrange(mean_x) %>%
+    # Calculate distance to next one
+    dplyr::mutate(dist_to_next = dplyr::lead(x = mean_x) - mean_x) %>%
+    # Add half the distance between +/0 and 0/- to +/0 to get +/- inflection point
+    dplyr::mutate(
+      pos_to_neg = ifelse(change_type == "change_to_zero" &
+                            dplyr::lead(change_type) == "change_to_negative",
+                          yes = (mean_x + (dist_to_next / 2)),
+                          no = NA) ) %>%
+      # Make it a dataframe
+      as.data.frame()
+
+  # Return that data object
+    return(sizer_data)
+}
+
+# Test the function
+fxn_test <- sizer_extract(sizer_x = priscu$Year, sizer_y = priscu$FNYield, deriv = 1)
+fxn_test
+
+# Create the demo plot
+ggplot(priscu, aes(x = Year, y = FNYield)) +
+  geom_point() +
+  geom_smooth(method = 'loess', formula = 'y ~ x',
+              se = F, color = 'black') +
+  geom_vline(xintercept = fxn_test$pos_to_neg, color = 'blue') +
+  geom_vline(xintercept = fxn_test$mean_x, color = 'orange') +
+  theme_classic()
 
 
 
