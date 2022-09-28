026b9ad9e9ea978411e99c3992a567f058ca582e
njlyon0 <nicholasjlyon@gmail.com>
2022-09-28 11:04:54 -0700

    Adding initial housekeeping part of ecocomDP exploration script

---
+++ wg_emergent/explore_ecocomDP.R
@@ -0,0 +1,27 @@
+## --------------------------------------------- ##
+              # Exploring `ecocomDP`
+## --------------------------------------------- ##
+# Follows this tutorial:
+# https://www.neonscience.org/resources/learning-hub/tutorials/neon-biodiversity-ecocomdp-cyverse
+
+# Clear environment
+rm(list = ls())
+
+## --------------------------------------------- ##
+# Housekeeping -----
+## --------------------------------------------- ##
+# Load needed packages
+# install.packages("librarian")
+librarian::shelf(neonUtilities, tidyverse, ecocomDP, vegan)
+
+# Load NEON token
+source(file.path("wg_emergent", "neon_token.R"))
+## "neon_token.R" creates "neon_token" object of the NEON token string
+
+
+
+
+
+
+
+# End ----
