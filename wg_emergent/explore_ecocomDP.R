5da595627270ecea1402f2aecc1094b5ec8b5700
njlyon0 <nicholasjlyon@gmail.com>
2022-09-28 11:09:52 -0700

    Added check for whether the token is successfully attached

--- wg_emergent/explore_ecocomDP.R
+++ wg_emergent/explore_ecocomDP.R
@@ -18,8 +18,10 @@ librarian::shelf(neonUtilities, tidyverse, ecocomDP, vegan)
 source(file.path("wg_emergent", "neon_token.R"))
 ## "neon_token.R" creates "neon_token" object of the NEON token string
 
-
-
+# Check token loading
+if(!exists("neon_token")){
+  message("NEON token *NOT* found! Attach token before continuing")
+} else { message("NEON token found! Continue") }
 
 
 
