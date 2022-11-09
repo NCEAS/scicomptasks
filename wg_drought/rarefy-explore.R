e6bffc8a2c93a7f49c659a1f0ba87bd497bfbdee
njlyon0 <nicholasjlyon@gmail.com>
2022-11-09 10:50:05 -0500

    Initial commit of rarefaction exploration script

---
+++ wg_drought/rarefy-explore.R
@@ -0,0 +1,21 @@
+## ----------------------------------------------------- ##
+              # Rarefy Package Exploration
+## ----------------------------------------------------- ##
+
+## ------------------------------------- ##
+# Housekeeping
+## ------------------------------------- ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, Rarefy, ade4, adiv, ape, vegan, phyloregion, raster)
+
+# Clear environment
+rm(list = ls())
+
+
+
+
+
+
+# End ----
