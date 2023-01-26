b173bcb7d8677fcdc9b5b2065b7d4bca5ec3121c
njlyon0 <nicholasjlyon@gmail.com>
2023-01-26 10:33:50 -0500

    First commit of NEON miscellaneous folder

---
+++ misc_neon/README.md
@@ -0,0 +1,7 @@
+# Handling NEON Data with R
+
+## Script explanation
+
+**"neon_tutorial.R"**
+
+- R script for all NEON tutorials performed on general principles (rather than specifically for a working group). More detailed information (including links to specific tutorials) can be found at the top of the script
---
+++ misc_neon/neon_tutorial.R
@@ -0,0 +1,43 @@
+## -------------------------------------------------------------- ##
+                # Neon Tutorial(s)
+## -------------------------------------------------------------- ##
+
+# This script is used for the following tutorials:
+### 1) Download and Explore NEON Data
+# https://www.neonscience.org/resources/learning-hub/tutorials/download-explore-neon-data
+### 2) Introduction to Small Mammal Data
+# https://www.neonscience.org/resources/learning-hub/tutorials/mammal-data-intro
+
+## -------------------------------------------- ##
+            # NEON Tutorial No. 1 ----
+## -------------------------------------------- ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(neonUtilities, neonOS, raster)
+
+# Clear environment
+rm(list = ls())
+
+
+
+
+
+
+## -------------------------------------------- ##
+# NEON Tutorial No. 2 ----
+## -------------------------------------------- ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(neonUtilities, neonOS, tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+
+# Workshop offered 1/31/23, will fill this part out then
+
+
+
+# End ----
