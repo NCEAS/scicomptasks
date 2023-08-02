9168c97eb27ef96010e8094f17f80dc34a992a87
njlyon0 <nicholasjlyon@gmail.com>
2023-08-02 09:56:01 -0400

    Finished getting data from Drive

--- misc_lter/lter_site_faq.R
+++ misc_lter/lter_site_faq.R
@@ -14,13 +14,29 @@
 
 # Load libraries
 # install.packages("librarian")
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, googledrive, purrr)
 
 # Clear environment
 rm(list = ls())
 
+# Authorize googledrive
+googledrive::drive_auth()
 
+## ------------------------------ ##
+# Data Acquisition ----
+## ------------------------------ ##
 
+# List all files in relevant Drive folder
+site_drive <- googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/12rNziwHWrnBp-hjSraAqL-9M-eXi7MTT")) %>%
+  # Filter to only desired files
+  dplyr::filter(name %in% c("site-start-end-dates-only.csv", "Site-list-2019.csv"))
 
+# Create a folder for these locally
+dir.create(path = file.path("data"), showWarnings = F)
+
+# Download both files
+purrr::walk2(.x = site_drive$name, .y = site_drive$id,
+             .f = ~ googledrive::drive_download(file = .y, overwrite = T,
+                                                path = file.path("data", .x)))
 
 # End ----
