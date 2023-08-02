dfed8ff9bee56a48af6ac3d17ae45467301cd098
njlyon0 <nicholasjlyon@gmail.com>
2023-08-02 10:36:47 -0400

    Finished creation of timeline (first draft)

--- misc_lter/lter_site_faq.R
+++ misc_lter/lter_site_faq.R
@@ -40,7 +40,9 @@ purrr::walk2(.x = site_drive$name, .y = site_drive$id,
                                                 path = file.path("data", .x)))
 
 # Read in data
-site_df <- read.csv(file = file.path("data", "site_faq_info.csv"))
+site_df <- read.csv(file = file.path("data", "site_faq_info.csv")) %>%
+  # Rename site name column
+  dplyr::rename(site_name = name)
 
 # Check structure
 dplyr::glimpse(site_df)
@@ -59,8 +61,46 @@ site_actual <- site_df
 # Site Timeline ----
 ## ------------------------------ ##
 
+# Flip site data to long format
+site_long <- site_actual %>%
+  tidyr::pivot_longer(cols = ends_with("_year"),
+                      values_to = "year")
+
+# Check structure
+dplyr::glimpse(site_long)
+
+# Get vector of habitat colors
+habitat_colors <- c("Coastal" = "#34a0a4", 
+                    "Freshwater" = "#48cae4", 
+                    "Marine" = "#1e6091", 
+                    "Forest" = "#007200",
+                    "Grassland" = "#9ef01a", 
+                    "Mixed Landscape" = "#7f5539",
+                    "Tundra" = "#bb9457", 
+                    "Urban" = "#9d4edd")
+
+# Make timeline
+timeline <- ggplot(site_long, aes(x = year, y = code)) +
+  geom_path(aes(group = code, color = habitat), lwd = 1, lineend = 'round') +
+  geom_point(aes(fill = habitat), pch = 21, size = 2) +
+  # Custom color
+  scale_fill_manual(values = habitat_colors) +
+  scale_color_manual(values = habitat_colors) +
+  # Customize theme elements
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_text(size = 12),
+        legend.title = element_blank()); timeline
+
 ## ------------------------------ ##
 # Site Map ----
+## ------------------------------ ##
+
+
+
+## ------------------------------ ##
+# Export ----
 ## ------------------------------ ##
 
 # End ----
