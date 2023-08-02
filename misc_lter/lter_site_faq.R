05a81668b65109e41c69d414576266e29f8b6af0
njlyon0 <nicholasjlyon@gmail.com>
2023-08-02 09:41:24 -0400

    First commit for LTER FAQ script

---
+++ misc_lter/lter_site_faq.R
@@ -0,0 +1,26 @@
+## -------------------------------------------------- ##
+                 # LTER Site FAQ
+## -------------------------------------------------- ##
+# Script author(s): Nick J Lyon
+
+# PURPOSE:
+## Wrangle LTER site information and create frequently-requested products
+## LTER = Long Term Ecological Research
+## FAQ = Frequently Asked Questions
+
+## ------------------------------ ##
+        # Housekeeping ----
+## ------------------------------ ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+
+
+
+
+# End ----
