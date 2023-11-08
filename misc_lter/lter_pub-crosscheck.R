36530ef8820a6428366916c47b516265e272688f
njlyon0 <nicholasjlyon@gmail.com>
2023-11-08 10:09:01 -0500

    Tweaked wrangling to be able to create graph 3 (and drafted graph 2)

--- misc_lter/lter_pub-crosscheck.R
+++ misc_lter/lter_pub-crosscheck.R
@@ -67,6 +67,8 @@ lter_v1 <- lter_v0 %>%
   # Pare LTER pubs down to just the desired columns
   dplyr::select(Item.Type, dplyr::starts_with("Publication."), 
                 Author, Title, ISBN:DOI) %>% 
+  # Add a column indicating which library these publications are in
+  dplyr::mutate(library = "LTER", .before = dplyr::everything()) %>% 
   # Make empty cells true NAs (across all columns)
   dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
                               .fns = ~ ifelse(test = nchar(.x) == 0,
@@ -90,6 +92,7 @@ dplyr::glimpse(lter_v1)
 neon_v1 <- neon_v0 %>% 
   dplyr::select(Item.Type, dplyr::starts_with("Publication."), 
                 Author, Title, ISBN:DOI) %>% 
+  dplyr::mutate(library = "NEON", .before = dplyr::everything()) %>% 
   dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
                               .fns = ~ ifelse(test = nchar(.x) == 0,
                                               yes = NA, no = .x))) %>% 
@@ -107,21 +110,28 @@ dplyr::glimpse(neon_v1)
     # Library Integration ----
 ## ------------------------------ ##
 
-# Time to combine the two for graphing purposes
+# Stack the publications into a single object
 combo_v1 <- lter_v1 %>% 
-  # Figure out whether each NEON publication was in the LTER publication list
+  dplyr::bind_rows(neon_v1) %>% 
+  # Identify whether each was in the other
   dplyr::mutate(shared = dplyr::case_when(
-    !is.na(Title) & Title %in% neon_v1$Title ~ 1,
-    !is.na(ISBN) & ISBN %in% neon_v1$ISBN ~ 1,
-    !is.na(ISSN) & ISSN %in% neon_v1$ISSN ~ 1,
-    !is.na(DOI) & DOI %in% neon_v1$DOI ~ 1,
+    # LTER pubs in NEON
+    library == "lter" & !is.na(Title) & Title %in% neon_v1$Title ~ 1,
+    library == "lter" & !is.na(ISBN) & ISBN %in% neon_v1$ISBN ~ 1,
+    library == "lter" & !is.na(ISSN) & ISSN %in% neon_v1$ISSN ~ 1,
+    library == "lter" & !is.na(DOI) & DOI %in% neon_v1$DOI ~ 1,
+    # NEON pubs in LTER
+    library == "neon" & !is.na(Title) & Title %in% lter_v1$Title ~ 1,
+    library == "neon" & !is.na(ISBN) & ISBN %in% lter_v1$ISBN ~ 1,
+    library == "neon" & !is.na(ISSN) & ISSN %in% lter_v1$ISSN ~ 1,
+    library == "neon" & !is.na(DOI) & DOI %in% lter_v1$DOI ~ 1,
     # Otherwise, assume not in publication list
     T ~ NA)) %>% 
   # Tweak the name of two key visualization columns
   dplyr::rename(type = Item.Type,
                 pub_year = Publication.Year) %>% 
   # Count instances per year and publication type
-  dplyr::group_by(type, pub_year) %>% 
+  dplyr::group_by(library, type, pub_year) %>% 
   dplyr::summarize(total_ct = dplyr::n(),
                    shared_ct = sum(shared, na.rm = T)) %>% 
   dplyr::ungroup() %>% 
@@ -131,24 +141,31 @@ combo_v1 <- lter_v1 %>%
 # Check structure
 dplyr::glimpse(combo_v1)
 
-# Make a version for shared only
+# Make a version for shared only (LTER focal)
 shared_only <- combo_v1 %>% 
-  dplyr::filter(shared_ct != 0)
+  dplyr::filter(library == "lter" & shared_ct != 0)
 
 ## ------------------------------ ##
             # Visuals ----
 ## ------------------------------ ##
 
-# Graph 1 - Shared paper count over time
+# Graph 1 - Shared paper *count* over time
 ggplot(shared_only, aes(x = pub_year, y = shared_ct)) +
   geom_smooth(method = "loess", formula = "y ~ x", se = F, color = 'black') +
   geom_point(aes(fill = shared_ct), shape = 21, size = 3) +
-  # Custom aesthetics
   labs(y = "Shared Publication Count", x = "Publication Year") +
   supportR::theme_lyon() +
   theme(legend.position = "none")
 
+# Graph 2 - Shared paper *proportion* over time
+ggplot(shared_only, aes(x = pub_year, y = shared_prop)) +
+  geom_smooth(method = "loess", formula = "y ~ x", se = F, color = 'black') +
+  geom_point(aes(fill = shared_prop), shape = 21, size = 3) +
+  labs(y = "Shared Publication Proportion", x = "Publication Year") +
+  supportR::theme_lyon() +
+  theme(legend.position = "none")
 
-# Graph 2 - 
+
+
 
 # End ----
