a32d6fdac41ef819eba3c3442a5d400ae072c914
njlyon0 <nicholasjlyon@gmail.com>
2024-03-28 14:06:50 -0500

    First bits of site shapefile script

---
+++ lter-network/lter_site-polygons.R
@@ -0,0 +1,27 @@
+## -------------------------------------------------- ##
+# LTER Site Polygons
+## -------------------------------------------------- ##
+# Script author(s): Nick J Lyon
+
+# PURPOSE:
+## Wrangle LTER site polygons into a single shapefile
+
+## ------------------------------ ##
+        # Housekeeping ----
+## ------------------------------ ##
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, googledrive, sf)
+
+# Clear environment
+rm(list = ls())
+
+
+
+
+
+
+
+
+# End ----
