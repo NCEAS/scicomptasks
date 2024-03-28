b3dd666fc462aa3f50d7b0b3b469a7d6b528e849
njlyon0 <nicholasjlyon@gmail.com>
2024-03-28 14:18:31 -0500

    Initial fleshing out of network polygon wrangling / BLE wrangling

--- lter-network/lter_site-polygons.R
+++ lter-network/lter_site-polygons.R
@@ -17,11 +17,41 @@ librarian::shelf(tidyverse, googledrive, sf)
 # Clear environment
 rm(list = ls())
 
+## ------------------------------ ##
+ # 2024 Consolidation Effort ----
+## ------------------------------ ##
 
+# Read in 2017 shapefiles
+lter_v1 <- sf::st_read(dsn = file.path("data", "lterDomains_110410.shp"))
 
+# Check contents
+dplyr::glimpse(lter_v1)
 
+# How many sites are included / which of them?
+sort(unique(lter_v1$SITE)); length(unique(lter_v1$SITE))
 
+# And new 2019 BLE polygons
+ble_v1 <- sf::st_read(dsn = file.path("data", "ble_lagoons_polygons.shp"))
 
+# Check contents
+dplyr::glimpse(ble_v1)
+
+# Wrangle BLE polygons for consistency with other polygons
+ble_v2 <- ble_v1 %>% 
+  # Drop unwanted column(s)
+  dplyr::select(-Id) %>% 
+  # Rename desired but inconsistent ones
+  dplyr::rename(SITE = Site,
+                NAME = Name)
+
+# Re-check
+dplyr::glimpse(ble_v2)
+
+# Attach BLE to the rest of the network
+lter_v2 <- dplyr::bind_rows(lter_v1, ble_v2)
+
+# Exploratory plot 
+plot(lter_v2["SITE"], axes = T)
 
 
 # End ----
