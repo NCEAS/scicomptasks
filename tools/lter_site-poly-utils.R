e69570785df2451e377c37104c4646616c200b86
njlyon0 <nicholasjlyon@gmail.com>
2024-07-10 10:26:08 -0400

    First pass at function (including errors/warnings)

--- tools/lter_site-poly-utils.R
+++ tools/lter_site-poly-utils.R
@@ -6,40 +6,55 @@
 # Polygon Tidying ----
 ## -------------------------------- ##
 
+# This function accepts a basically raw (but still 'sf') boundary for a particular site and readies it for inclusion in the network-wide set of polygons
 poly_tidy <- function(site_sf = NULL, network_sf = NULL,
                       code = NULL, name = NULL, plot = FALSE){
   
+  # Error out if any arguments are undefined
+  if(any(is.null(site_sf) | is.null(network_sf) | is.null(code) | is.null(name)) == TRUE)
+    stop("All arguments must be defined")
   
+  # Error out for inappropriate spatial class
+  if(all(class(site_sf) %in% c("sf", "data.frame")) != TRUE)
+    stop("Site-specific boundary must be supplied as a simple features ('sf') object")
+  if(all(class(network_sf) %in% c("sf", "data.frame")) != TRUE)
+    stop("Network boundaries must be supplied as a simple features ('sf') object")
   
+  # Error out for inappropriate code/name class
+  if(is.character(code) != TRUE | is.character(name) != TRUE)
+    stop("Site code and full name must be supplied as characters")
   
+  # Error out for malformed site code
+  if(nchar(code) != 3)
+    stop("Site code must be the three-letter abbreviation for the site.")
   
-}
+  # Warn and fix for malformed 'plot' logical
+  if(is.logical(plot) != TRUE){
+    warning("'plot' must be a logical, defaulting to FALSE")
+    plot <- FALSE }
+  
+  # Actually do the needed wrangling
+  site_actual <- site_sf %>% 
+    # Transform CRS (it may be correct already but better safe than sorry)
+    sf::st_transform(x = ., crs = sf::st_crs(network_sf)) %>% 
+    # Add in desired columns
+    dplyr::mutate(SITE = code, NAME = name) %>% 
+    # Drop all columns except those new ones
+    dplyr::select(SITE, NAME)
+  
+  # Return that object
+  return(site_actual)
+  
+  # If plotting is desired, make a simple plot
+  if(plot == TRUE){
+    plot(site_actual["SITE"], axes = TRUE)
+  }
+    
+} # Close function
 
 
-# Read in NGA GeoJSON and transform to sf
-arc_v1 <- geojsonio::geojson_read(x = file.path("data", "ARCLTER_bounday_2024.geojson"),
-                                  what = "sp") %>%
-  sf::st_as_sf(x = .)
 
-# Glimpse it
-dplyr::glimpse(arc_v1)
 
-# Wrangle NGA polygons for consistency with other polygons
-arc_v2 <- arc_v1 %>% 
-  # Transform CRS (is already right but better safe than sorry)
-  sf::st_transform(x = ., crs = sf::st_crs(lter_v1)) %>% 
-  # Add in desired columns
-  dplyr::mutate(SITE = "ARC",
-                NAME = "Arctic",
-                .before = dplyr::everything()) %>% 
-  # Pare down to just the desired columns
-  dplyr::select(SITE, NAME)
-
-# Check the structure of that
-dplyr::glimpse(arc_v2)
-
-# Visual demo
-plot(arc_v2["SITE"], axes = T)
 
 # End ----
 
